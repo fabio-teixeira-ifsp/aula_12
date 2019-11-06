32074da876dab000d138fa3247d436a0a360946f
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2019-11-06 17:29:28 -0300

    Updates

---
+++ precache-manifest.3bcd25368900b4435557b93b84a9bfa5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2d78e4d1f43922da1340593fc95c49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2e708429a864504cc76",
+    "url": "/static/js/2.048505da.chunk.js"
+  },
+  {
+    "revision": "58daef6ed77ddab9fe5f",
+    "url": "/static/js/main.88432ba0.chunk.js"
+  },
+  {
+    "revision": "5bb599adba6aec5077d3",
+    "url": "/static/js/runtime-main.c2da910f.js"
+  }
+]);
